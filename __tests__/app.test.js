c41daad486cbff7789e09cc4d0f49eb88cf424ee
Tanner Seramur <seramurt@gmail.com>
2018-12-28 09:33:49 -0800

    push

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,59 +3,16 @@
 const alterFile = require('../file-writer');
 const logger = require('../logger');
 
-// describe('running tests on alterFile', ()=>{
-//     it('test', ()=>{
-//         let expected =jest.spyOn( console, 'text.txt saved' ); 
-//         let result = alterFile('text.txt');
-//         // console.log(result, 'HERE💀💀💀💀💀💀💀💀💀💀');
-//         expect(result).toHaveLogged(expected);
-//     });
-// })
-
-describe('running tests on event handlers', ()=>{
+describe('running tests on alterFile', ()=>{
     it('test', ()=>{
-
-        let errorHandlerSpy;
-  let savedSpy;
-  let successSpy;
-
-  beforeAll(() => {
-    errorHandlerSpy = jest.spyOn(logger, 'errorHandler');
-    successSpy = jest.spyOn(logger, 'success');
-    savedSpy = jest.spyOn(logger, 'saved');
-  });
-
-  //happy path
-  it('reads a file', () => {
-    alterFile(__dirname+'/test.file.txt');
-    expect(successSpy).toHaveBeenCalled();
-  })
-
-  it('changes a file to upper case', () => {
+        let spy =jest.spyOn( console, 'log' ); 
+        event.savedLog('text.txt');
+        saved.emit('saved', 'test.txt');
+        expect(spy).toHaveBeenCalled();
+        spy.mockRestore();
+        // console.log(result, 'HERE💀💀💀💀💀💀💀💀💀💀');
     
-  })
-
-  it('writes a file', () => {
-    
-  })
-
-  it('requires a file name to read', () => {
-    
-  })
-
-  afterAll(() => {
-    errorHandlerSpy.mockRestore();
-    successSpy.mockRestore();
-    savedSpy.mockRestore();
-  });
-        
-        // // expect.assertions(1);
-        // // console.log( 'xsLOGER HEREEEEEEEEEEEEEEEE');
-        // const spy = jest.spyOn(logger, 'savedd');
-        // let result = logger.savedd();
-
-
-        // expect(spy).toHaveBeenCalled();
-        // expect(result).toBe(true);
     });
 })
+
+   
